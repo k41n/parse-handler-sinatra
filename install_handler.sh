50ab8dd5903171f590430f3cec8e3d803637c34d
Andrei Malyshev <Andrei.Malyshev@kodep.ru>
2015-11-26 09:37:39 +0300

    Remove sensitive information from commit

--- install_handler.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-
-curl -X POST \
-  -H "X-Parse-Application-Id: XMZz2SGfMLfsYJx3DL5Wy1QPcDpPlkxF4t099EAY" \
-  -H "X-Parse-Master-Key: HtuzCk2bDCuQ6sEs4aJp9wtFTz1ca8R6vYyP8Czv" \
-  -H "Content-Type: application/json" \
-  -d '{"className": "_User", "triggerName": "afterSave", "url": "https://parsecom-aweber-bridge.herokuapp.com/api/parsecom/bahh0VeiPhoa"}' \
-https://api.parse.com/1/hooks/triggers
--- uninstall_handler.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-
-curl -X PUT \
-  -H "X-Parse-Application-Id: XMZz2SGfMLfsYJx3DL5Wy1QPcDpPlkxF4t099EAY" \
-  -H "X-Parse-Master-Key: HtuzCk2bDCuQ6sEs4aJp9wtFTz1ca8R6vYyP8Czv" \
-  -H "Content-Type: application/json" \
-  -d '{ "__op": "Delete" }' \
-https://api.parse.com/1/hooks/triggers/_User/beforeSave
